c2d82814c225a736aa90d4126cbfe3f680c14bd0
Charleneeid11 <charlene.elkhouryeid@gmail.com>
2024-05-03 21:50:50 +0300

    add: sidebar with logo and closing button

--- app/Components/Header.tsx
+++ app/Components/Header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import NavMenu from "../(home)/components/NavMenu";
 import HamburgerMenu from "./hamburgerMenu";
@@ -6,21 +7,59 @@ import HamburgerMenu from "./hamburgerMenu";
 type Props = {};
 
 export default function Header({}: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="p-4 px-8 flex items-center justify-between"> 
-      <Image src="/icons/logo.svg" alt="Logo" width={250} height={250} className="w-[250px] max-md:w-[200px] max-sm:w-[150px]" />
+    <div className="p-4 px-8 flex items-center justify-between">
+      <Image
+        src="/icons/logo.svg"
+        alt="Logo"
+        width={250}
+        height={250}
+        className="w-[250px] max-md:w-[200px] max-sm:w-[150px]"
+      />
       {/* large screens show nav menu */}
       <NavMenu />
       {/* small screens have sidebar with hamburger menu */}
-      <HamburgerMenu/>
+      <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+      <div
+        className={`${
+          isOpen ? "" : "-translate-x-[100vw]"
+        } transition-all w-[45%] ease-in-out duration-500 h-[100vh] bg-[#ECD8BD] absolute left-0 top-0 z-20 md:hidden`}
+      >
+        <div className="flex justify-between items-center py-4 px-4">
+          <Image
+            src="/icons/sideBarLogo.png"
+            alt="logo"
+            width={100}
+            height={100}
+          />
+          <Image
+            src="/icons/close.png"
+            alt="hamburger"
+            width={15}
+            height={15}
+            className="w-[15px] h-[15px]"
+            onClick={() => setIsOpen(false)}
+          />
+        </div>
+      </div>
+      <div
+        onClick={() => setIsOpen(false)}
+        className={`z-10 absolute left-0 top-0 ${
+          isOpen ? "w-[100%]" : "w-0"
+        } transition-all ease-in-out duration-200 h-[100vh] bg-black opacity-50 md:hidden`}
+      >
+        
+      </div>
     </div>
   );
 }
 
 //flex: The element becomes a flex container, and its direct children become flex items.
-//justify-between: It distributes the items in such a way that the first item is at the start line, 
-//the last item is at the end line, and the remaining items are distributed so that the space between 
-//any two items is the same. It ensures that there's equal spacing between each flex item, 
+//justify-between: It distributes the items in such a way that the first item is at the start line,
+//the last item is at the end line, and the remaining items are distributed so that the space between
+//any two items is the same. It ensures that there's equal spacing between each flex item,
 //but not between the items and the container edges.
-//items-center: vertically centers the flex items in the container 
+//items-center: vertically centers the flex items in the container
 //(if the flex direction is row-wise) or horizontally (if the flex direction is column-wise)
--- app/Components/hamburgerMenu.tsx
+++ app/Components/hamburgerMenu.tsx
@@ -2,42 +2,34 @@
 import React, { useState } from "react";
 import Image from "next/image";
 
-type Props = {};
+type Props = {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+};
 
-const HamburgerMenu = (props: Props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
+const HamburgerMenu = ({ isOpen, setIsOpen }: Props) => {
   return (
-    <button
-      className={`md:hidden flex flex-col gap-1`}
-      onClick={() => setIsOpen(!isOpen)}
-    >
-      {/* hamburger */}
-      <div className="h-[60px] w-[60px] flex items-center justify-center translate-y-6">
-        <Image
-          src="/icons/hamburger.png"
-          alt="hamburger"
-          width={25}
-          height={25}
-          className={`${
-            isOpen ? "w-[0px]" : "w-[25px]"
-          } h-[25px] ease-in-out duration-500`}
-        />
-      </div>
-
-      {/* close button */}
-      <div className="h-[60px] w-[60px] flex items-center justify-center -translate-y-10">
-        <Image
-          src="/icons/close.png"
-          alt="hamburger"
-          width={25}
-          height={25}
-          className={`${
-            isOpen ? "w-[25px] h-[25px]" : "w-[0px] h-[0px]"
-          } ease-in-out duration-500`}
-        />
-      </div>
-    </button>
+    <div className="relative md:hidden">
+      {/* open button */}
+      <button
+        className={`md:hidden flex flex-col gap-1`}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {/* hamburger */}
+        <div className="h-[60px] w-[60px] flex items-center justify-center">
+          <Image
+            src="/icons/hamburger.png"
+            alt="hamburger"
+            width={25}
+            height={25}
+            className={`${
+              isOpen ? "w-[0px]" : "w-[25px]"
+            } h-[25px] transition-all ease-in-out duration-500`}
+          />
+        </div>
+      </button>
+      
+    </div>
   );
 };
 
